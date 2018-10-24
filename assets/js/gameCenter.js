6d5b828f2c143c3f1a576796436f51db901c27a3
Caillou <Boucher.Pierre@hotmail.fr>
2018-10-24 11:45:38 +0200

    Banissement des majuscules en début de variable terminé

--- assets/js/gameCenter.js
+++ assets/js/gameCenter.js
@@ -20,14 +20,14 @@ $(function(){
   $('#registrationForm').click(function(){
     pseudonyme = $('#pseudo').val();
     Name = $('#name').val();
-    FirstName = $('#firstName').val();
+    firstName = $('#firstName').val();
     mail = $('#mail').val();
     confirmMail = $('#confirmMail').val();
     password = $('#password').val();
     confirmPassword = $('#confirmPassword').val();
     address = $('#address').val();
-    City = $('#city').val();
-    PostalCode = $('#postalCode').val();
+    city = $('#city').val();
+    postalCode = $('#postalCode').val();
     birthDate = $('#birthDate').val();
     regexAddress = /^\d{1,5}\,{0,1}\s+[\sa-zA-Z\-]{1,}$/;
     regexName = /^[a-zA-Z\-\séèÉë]{1,}$/;
@@ -35,7 +35,7 @@ $(function(){
     regexPass = /^[a-zA-Z0-9]{8,}$/;
     regexPost = /^[0-9]{5,}$/;
     regexBd = /^[0-3][0-9]\/[0-1][0-9]\/[1-2][09][0-9]{2}$/;
-    if (regexMail.test(mail) && regexName.test(Name) && regexAddress.test(address) && regexName.test(FirstName) && regexName.test(pseudonyme) && regexPass.test(password) && password === confirmPassword && mail === confirmMail && regexPost.test(PostalCode) && regexName.test(City) && regexBd.test(birthDate)){
+    if (regexMail.test(mail) && regexName.test(Name) && regexAddress.test(address) && regexName.test(firstName) && regexName.test(pseudonyme) && regexPass.test(password) && password === confirmPassword && mail === confirmMail && regexPost.test(postalCode) && regexName.test(city) && regexBd.test(birthDate)){
       alert('Merci pour votre achat');
       $('#closeModalBtn').click(function(e) {
         e.preventDefault();
@@ -58,7 +58,7 @@ $(function(){
       } else {
         $('#address').removeClass('RED');
       }
-      if (regexName.test(FirstName) == false) {
+      if (regexName.test(firstName) == false) {
         $('#firstName').addClass('RED');
       } else {
         $('#firstName').removeClass('RED');
@@ -83,12 +83,12 @@ $(function(){
       } else {
         $('#confirmMail').removeClass('RED');
       }
-      if (regexPost.test(PostalCode) == false) {
+      if (regexPost.test(postalCode) == false) {
         $('#postalCode').addClass('RED');
       } else {
         $('#postalCode').removeClass('RED');
       }
-      if (regexName.test(City) == false) {
+      if (regexName.test(city) == false) {
         $('#city').addClass('RED');
       } else {
         $('#city').removeClass('RED');
